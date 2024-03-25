e4e26f21ab7aed632304ba9bebbffc3ac52bb826
Rob Fielding <rob@g17.co.nz>
2024-03-25 14:37:52 +1300

    setting up lsp

--- nvim/lua/lspconfig/ruff-lsp.lua
+++ nvim/lua/lspconfig/ruff-lsp.lua
@@ -1,1 +1,8 @@
-pylsp.plugins.ruff.format = { "I" }
+require("lspconfig").ruff_lsp.setup({
+	init_options = {
+		settings = {
+			-- Any extra CLI arguments for `ruff` go here.
+			args = {},
+		},
+	},
+})
