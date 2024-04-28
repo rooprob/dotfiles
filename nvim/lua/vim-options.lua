a84fa731338fc691262e7d9f2cdf9fda9088317b
Rob Fielding <rob@g17.co.nz>
2024-04-28 21:54:29 +1200

    vim keymaps for resizing

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -2,4 +2,11 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+
+vim.keymap.set("i", "jk", "<ESC>", { silent = true })
+-- https://www.reddit.com/r/neovim/comments/10wru1c/how_do_i_resize_windows/
+vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
+vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
+vim.keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
+vim.keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
 vim.g.mapleader = " "
