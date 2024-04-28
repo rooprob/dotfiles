760879109cad187d4a7a792f19d9fd6ebc9b1021
Rob Fielding <rob@g17.co.nz>
2024-04-28 17:19:50 +1200

    lift vim options

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.vim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -17,6 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local opts = { }
 
-require("lazy").setup("plugins", opts)
+require("vim-options")
+require("lazy").setup("plugins")
