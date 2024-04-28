94b1c7e2a2fedd42115a511c9d5cb812dd3e2974
Rob Fielding <rob@g17.co.nz>
2024-04-28 21:52:25 +1200

    add fzf

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,8 @@
 return { 
     'nvim-telescope/telescope.nvim', 
     branch = '0.1.x',
-    dependencies = {'nvim-lua/plenary.nvim'} ,
+    dependencies = {'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope-fzf-native.nvim'} ,
     config = function()
         local builtin = require("telescope.builtin")
         vim.keymap.set('n', '<C-p>', builtin.find_files, {})
