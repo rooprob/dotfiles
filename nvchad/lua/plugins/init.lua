a3270dd7381f6a88cf1398a0ae923fd53b140e8a
Rob Fielding <rob@g17.co.nz>
2024-12-27 23:55:54 +1300

    add nvchad

---
+++ nvchad/lua/plugins/init.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre", -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
+
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"css",
+				"go",
+				"json",
+				"html",
+				"lua",
+				"vim",
+				"vimdoc",
+			},
+		},
+	},
+}
