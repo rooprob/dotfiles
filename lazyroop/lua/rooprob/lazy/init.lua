d671e5228910863f3e9cf60ab90968cbaab61ebe
Rob Fielding <rob@g17.co.nz>
2024-12-28 12:03:59 +1300

    actually become nvchad now ... review bufuline and ui modules to undo

--- lazyroop/lua/rooprob/lazy/init.lua
+++ lazyroop/lua/rooprob/lazy/init.lua
@@ -7,6 +7,22 @@ return {
     },
 
     
+    {
+        "nvchad/base46",
+        build = function()
+            require("base46").load_all_highlights()
+        end,
+    },
+
+
+    {
+        "nvchad/ui",
+        lazy = false,
+        config = function()
+            require "nvchad"
+        end,
+    },
+    
     -- Provides fuzzy finder over lists. 
     -- <leader>ff  find files
     -- <leader>fw  find word in a live grep
@@ -34,7 +50,7 @@ return {
                     height = 0.80,
                 },
                 mappings = {
-                    n = { ["q"] = require("telescope.actions").close },
+                        n = { ["q"] = require("telescope.actions").close },
                 },
             },
 
@@ -218,7 +234,28 @@ return {
                 opts = { history = true, updateevents = "TextChanged,TextChangedI" },
                 config = function(_, opts)
                     require("luasnip").config.set_config(opts)
-                    require "nvchad.configs.luasnip"
+                    -- vscode format
+                    require("luasnip.loaders.from_vscode").lazy_load { exclude = vim.g.vscode_snippets_exclude or {} }
+                    require("luasnip.loaders.from_vscode").lazy_load { paths = vim.g.vscode_snippets_path or "" }
+
+                    -- snipmate format
+                    require("luasnip.loaders.from_snipmate").load()
+                    require("luasnip.loaders.from_snipmate").lazy_load { paths = vim.g.snipmate_snippets_path or "" }
+
+                    -- lua format
+                    require("luasnip.loaders.from_lua").load()
+                    require("luasnip.loaders.from_lua").lazy_load { paths = vim.g.lua_snippets_path or "" }
+
+                    vim.api.nvim_create_autocmd("InsertLeave", {
+                        callback = function()
+                            if
+                                require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+                                and not require("luasnip").session.jump_active
+                            then
+                                require("luasnip").unlink_current()
+                            end
+                        end,
+                    })
                 end,
             },
 
